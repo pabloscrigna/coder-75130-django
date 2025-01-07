82ef36a21d3992873819648b9e079544de783379
Pablo Scrigna <pscrigna@gruposimpli.com>
2025-01-07 20:58:59 -0300

    clase 11 - Formularios

--- proyecto/appCoder/admin.py
+++ proyecto/appCoder/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Curso, Profesor
+
+@admin.register(Curso)
+class CursoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'camada', 'creado')
+    search_fields = ('nombre', 'camada')
+    list_filter = ('camada', 'creado')
+    ordering = ('creado',)
+
+@admin.register(Profesor)
+class ProfesorAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'apellido', 'edad', 'creado')
+    search_fields = ('nombre', 'apellido', 'edad')
+    list_filter = ('edad', 'creado')
+    ordering = ('creado',)
